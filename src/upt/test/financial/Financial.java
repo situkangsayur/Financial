81beea589301c637247b0f769af1a5f78e8ec493
Hendri Karisma <situkangsayur@gmail.com>
2012-01-06 16:55:05 +0700

    project all Financial

---
+++ src/upt/test/financial/Financial.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package upt.test.financial;
+
+/**
+ *
+ * @author h3ndr1
+ */
+public class Financial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World..");
+    }
+}
