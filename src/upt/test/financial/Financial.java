3f6a08b0e20d863bffb6c98ef44a2e4d7c3522c5
Hendri Karisma <situkangsayur@gmail.com>
2012-01-06 16:58:57 +0700

    nambah saya tono

--- src/upt/test/financial/Financial.java
+++ src/upt/test/financial/Financial.java
@@ -16,5 +16,6 @@ public class Financial {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World..");
+        System.out.println("Nama saya Tono");
     }
 }
